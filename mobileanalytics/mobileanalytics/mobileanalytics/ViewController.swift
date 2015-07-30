0be4b89fbc7a33ba64b3d8d4fe8991fadbbfe777
Laurie Keith <laurie.laptop@gmail.com>
2015-07-30 13:48:05 +0100

    First commit

---
+++ mobileanalytics/mobileanalytics/mobileanalytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobileanalytics
+//
+//  Created by Laurie Keith on 27/07/2015.
+//  Copyright (c) 2015 EMIS Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
