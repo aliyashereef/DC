ead124d96e2e725dacec428ff5bc9fafa553911f
Jilu Mary Joy <jilu@qburst.com>
2015-09-28 18:26:15 +0530

    reduced administer segmented control width, changed administer date format

--- DrugChart/DrugChart/Constants/DCConstants.h
+++ DrugChart/DrugChart/Constants/DCConstants.h
@@ -209,7 +209,7 @@
 #define BIRTH_DATE_FORMAT               @"dd MMM yyyy"
 #define LONG_DATE_FORMAT                @"yyyy-MM-dd HH:mm:ss"
 #define DATE_MONTHNAME_YEAR_FORMAT      @"d LLLL yyyy"
-#define ADMINISTER_DATE_TIME_FORMAT     @"d-LL-yyyy hh:mm a"
+#define ADMINISTER_DATE_TIME_FORMAT     @"d-MMM-yyyy hh:mm a"
 
 #define SERVER_DATE_FORMAT       @"yyyy-MM-dd'T'hh:mm:ss"
 
