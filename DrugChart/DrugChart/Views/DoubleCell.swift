ab5b73e2542d13a44421f5d562d365c9bb2bd90a
noureenwajeeh <nwajeh@gmail.com>
2015-11-18 16:04:16 +0000

    make pop over calendar to the inline control
    also added the numeric validation

--- DrugChart/DrugChart/Views/DoubleCell.swift
+++ DrugChart/DrugChart/Views/DoubleCell.swift
@@ -92,69 +92,8 @@ class DoubleCell: UITableViewCell ,UITextFieldDelegate {
         
         return prospectiveText.containsOnlyCharactersIn("0123456789.") &&
             prospectiveText.characters.count <= 5
-        
-        
-//        switch textField {
-//            
-//            // Allow only upper- and lower-case vowels in this field,
-//            // and limit its contents to a maximum of 6 characters.
-//        case vowelsOnlyTextField:
-//            return prospectiveText.containsOnlyCharactersIn("aeiouAEIOU") &&
-//                prospectiveText.characters.count <= 6
-//            
-//            // Allow any characters EXCEPT upper- and lower-case vowels in this field,
-//            // and limit its contents to a maximum of 8 characters.
-//        case noVowelsTextField:
-//            return prospectiveText.doesNotContainCharactersIn("aeiouAEIOU") &&
-//                prospectiveText.characters.count <= 8
-//            
-//            // Allow only digits in this field,
-//            // and limit its contents to a maximum of 3 characters.
-//        case digitsOnlyTextField:
-//            return prospectiveText.containsOnlyCharactersIn("0123456789") &&
-//                prospectiveText.characters.count <= 3
-//            
-//            // Allow only values that evaluate to proper numeric values in this field,
-//            // and limit its contents to a maximum of 7 characters.
-//        case numericOnlyTextField:
-//            return prospectiveText.isNumeric() &&
-//                prospectiveText.characters.count <= 7
-//            
-//            // In this field, allow only values that evalulate to proper numeric values and
-//            // do not contain the "-" and "e" characters, nor the decimal separator character
-//            // for the current locale. Limit its contents to a maximum of 5 characters.
-//        case positiveIntegersOnlyTextField:
-//            let decimalSeparator = NSLocale.currentLocale().objectForKey(NSLocaleDecimalSeparator) as! String
-//            return prospectiveText.isNumeric() &&
-//                prospectiveText.doesNotContainCharactersIn("-e" + decimalSeparator) &&
-//                prospectiveText.characters.count <= 5
-//            
-//            // Do not put constraints on any other text field in this view
-//            // that uses this class as its delegate.
-//        default:
-//            return true
-//        }
     }
-//
-//    func addDoneButtonToKeyboard() {
-//        var doneButton:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "hideKeyboard")
-//        
-//        var space:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
-//        
-//        var items = [AnyObject]()
-//        items.append(space)
-//        items.append(doneButton)
-//        var toolbar = UIToolbar.new()
-//        
-//        toolbar.frame.size.height = 35
-//        
-//        toolbar.items = items
-//        
-//        value.inputAccessoryView = toolbar
-//    }
     
-//    func hideKeyboard() {
-//        value.resignFirstResponder()
-//    }
+    
 
 }
