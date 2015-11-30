96e773b91551b3f94bff07e4abfbe54c4169d1d3
muhammeds <muhammeds@qburst.com>
2015-11-30 14:55:29 +0530

    Fixed the 2/3rd to full screen switching crash issue

--- DrugChart/DrugChart/ViewControllers/PrescriberVC/DCCalendarDateDisplayViewController.swift
+++ DrugChart/DrugChart/ViewControllers/PrescriberVC/DCCalendarDateDisplayViewController.swift
@@ -119,6 +119,7 @@ import UIKit
     func displayDatesInView () {
         
         let displayDatesArray = displayWeekDatesArray()
+        print("the dates array : %@", displayDatesArray)
         var index : NSInteger = 0
         let leftDatesArray : NSMutableArray = []
         let centerDatesArray : NSMutableArray = []
@@ -126,6 +127,7 @@ import UIKit
         let calendarStripDaysCount = (appDelegate.windowState == DCWindowState.fullWindow) ? 5:3
         
         for ( index = 0; index < displayDatesArray.count; index++) {
+            print("the calendar strips count: %d", calendarStripDaysCount)
             if (index < calendarStripDaysCount) {
                 leftDatesArray.addObject(displayDatesArray.objectAtIndex(index))
             }
@@ -137,6 +139,7 @@ import UIKit
             }
         }
         print(centerCalendarView.backgroundColor)
+        print("the arrays: %@ %@ %@", leftDatesArray, centerDatesArray,rightDatesArray)
         leftCalendarView .populateViewForDateArray(leftDatesArray)
         centerCalendarView.populateViewForDateArray(centerDatesArray)
         rightCalendarView.populateViewForDateArray(rightDatesArray)
