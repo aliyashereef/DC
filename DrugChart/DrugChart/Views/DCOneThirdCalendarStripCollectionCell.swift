e6af68b3919defd20cf7404108a8da8b3aacf921
Jilu Mary Joy <jilu@qburst.com>
2015-12-08 14:38:18 +0530

    increased the font size for one third week view, increased diameter of indicator label

--- DrugChart/DrugChart/Views/DCOneThirdCalendarStripCollectionCell.swift
+++ DrugChart/DrugChart/Views/DCOneThirdCalendarStripCollectionCell.swift
@@ -11,6 +11,7 @@ import Foundation
 class DCOneThirdCalendarStripCollectionCell: UICollectionViewCell {
 
     var indicatorLabel: UILabel = UILabel()
+    var indicatorImageView : UIImageView = UIImageView()
     var displayDate : NSDate?
     @IBOutlet weak var weekdayLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
@@ -44,14 +45,14 @@ class DCOneThirdCalendarStripCollectionCell: UICollectionViewCell {
         dateLabel.textColor = UIColor.blackColor()
         dateFormatter.dateFormat = DAY_DATE_FORMAT as String
         let dateString = dateFormatter.stringFromDate(today)
-        indicatorLabel.frame = CGRectMake(0,0, 20, 20)
+        indicatorLabel.frame = CGRectMake(0,0, 28, 28)
         indicatorLabel.center =  dateLabel.center
-        indicatorLabel.font = UIFont.systemFontOfSize(13)
+        indicatorLabel.font = UIFont.systemFontOfSize(16)
         indicatorLabel.textAlignment = .Center
         indicatorLabel.textColor = UIColor.whiteColor()
         indicatorLabel.text = dateString
         indicatorLabel.backgroundColor = UIColor(forHexString: "#007aff")
-        indicatorLabel.layer.cornerRadius = 10
+        indicatorLabel.layer.cornerRadius = 14
         indicatorLabel.layer.masksToBounds = true
         self.addSubview(indicatorLabel)
         self.bringSubviewToFront(indicatorLabel)
@@ -76,14 +77,14 @@ class DCOneThirdCalendarStripCollectionCell: UICollectionViewCell {
         dateFormatter.dateFormat = DAY_DATE_FORMAT as String
         let dateString = dateFormatter.stringFromDate(displayDate!)
         dateLabel.textColor = UIColor.blackColor()
-        indicatorLabel.frame = CGRectMake(0,0, 20, 20)
+        indicatorLabel.frame = CGRectMake(0,0, 28, 28)
         indicatorLabel.center =  dateLabel.center
-        indicatorLabel.font = UIFont.systemFontOfSize(13)
+        indicatorLabel.font = UIFont.systemFontOfSize(16)
         indicatorLabel.textAlignment = .Center
         indicatorLabel.textColor = UIColor.whiteColor()
         indicatorLabel.text = dateString
         indicatorLabel.backgroundColor = UIColor(forHexString: "#1e1e1e")
-        indicatorLabel.layer.cornerRadius = 10
+        indicatorLabel.layer.cornerRadius = 14
         indicatorLabel.layer.masksToBounds = true
         self.addSubview(indicatorLabel)
         self.bringSubviewToFront(indicatorLabel)
