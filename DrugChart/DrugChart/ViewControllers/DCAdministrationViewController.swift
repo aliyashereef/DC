b64339fb23044765b774908e31a40a38372c7266
Aliya <aliya@qburst.com>
2015-12-17 18:24:14 +0530

    Added the administration table view.

--- DrugChart/DrugChart/ViewControllers/DCAdministrationViewController.swift
+++ DrugChart/DrugChart/ViewControllers/DCAdministrationViewController.swift
@@ -16,11 +16,17 @@ class DCAdministrationViewController : UIViewController {
     var contentArray :[AnyObject] = []
     var slotToAdminister : DCMedicationSlot?
     var weekDate : NSDate?
+    @IBOutlet var administerTableView: UITableView!
     var patientId : NSString = EMPTY_STRING
     var scheduleId : NSString = EMPTY_STRING
     var errorMessage : String = EMPTY_STRING
     var helper : DCSwiftObjCNavigationHelper = DCSwiftObjCNavigationHelper.init()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.administerTableView.rowHeight = UITableViewAutomaticDimension
+        self.administerTableView.estimatedRowHeight = 44.0
+    }
     
     func initialiseMedicationSlotToAdministerObject () {
         
@@ -35,6 +41,48 @@ class DCAdministrationViewController : UIViewController {
             }
         }
     }
+    //MARK: TableView Delegate Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    //The number of rows is determined by the medication slot status, if is administrated, the section will require 6 rows, if ommitted it may require 2 rows and 3 for the refused state.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 1 : return 1
+        case 2 : return medicationSlotsArray.count
+        default : break
+        }
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(ADMINSTER_MEDICATION_HISTORY_CELL) as? DCAdminsteredMedicationCell
+        if cell == nil {
+            cell = DCAdminsteredMedicationCell(style: UITableViewCellStyle.Value1, reuseIdentifier: ADMINSTER_MEDICATION_HISTORY_CELL)
+        }
+        
+        
+        return cell!
+    }
+    
+    // MARK: Header View Methods
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 1: return 0.0
+        case 2: return 44.0
+        default : break
+        }
+        return 0
+    }
+    
+    // returns the header view
+//    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//    }
     
     
 }
