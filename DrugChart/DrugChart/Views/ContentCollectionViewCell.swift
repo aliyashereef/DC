ea7ece4a42ce2b8ae26d2b54769c28846f559f35
noureenwajeeh <nwajeh@gmail.com>
2016-01-13 09:58:43 +0000

    add delete button on content cell

--- DrugChart/DrugChart/Views/ContentCollectionViewCell.swift
+++ DrugChart/DrugChart/Views/ContentCollectionViewCell.swift
@@ -16,10 +16,31 @@ class ContentCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+     
     }
     
     func configureCell(observation:VitalSignObservation ,showobservationType:ShowObservationType )
     {
+        // add the delete button 
+        if(showObservationType != ShowObservationType.None && showObservationType != ShowObservationType.All)
+        {
+            
+        let indicatorLabel: UILabel = UILabel()
+        let originx = self.frame.width - 25
+        indicatorLabel.frame = CGRectMake(originx, 4, 25, 25)
+        indicatorLabel.font = UIFont.systemFontOfSize(12)
+        indicatorLabel.textAlignment = .Center
+        indicatorLabel.text = "X"
+        indicatorLabel.layer.borderWidth = 0.5
+        indicatorLabel.layer.borderColor = UIColor.redColor().CGColor
+        indicatorLabel.textColor = UIColor.redColor()
+        indicatorLabel.backgroundColor = UIColor.whiteColor()
+        indicatorLabel.layer.cornerRadius = 14
+        indicatorLabel.layer.masksToBounds = true
+        self.addSubview(indicatorLabel)
+        }
+        // normal stuff
         self.observation = observation
         self.showObservationType = showobservationType
         let tap = UITapGestureRecognizer(target: self, action: "doubleTapped")
