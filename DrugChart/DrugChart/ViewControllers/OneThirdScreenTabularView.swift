f88e68d93d90d1f21d434db50c32a949aa05ded3
noureenwajeeh <nwajeh@gmail.com>
2016-01-27 16:33:54 +0000

    swipe delete functionality in iphone

--- DrugChart/DrugChart/ViewControllers/OneThirdScreenTabularView.swift
+++ DrugChart/DrugChart/ViewControllers/OneThirdScreenTabularView.swift
@@ -110,16 +110,34 @@ class OneThirdScreenTabularView: UIViewController,UICollectionViewDataSource, UI
         let count  = ObservationTabularViewRow.count - 1
         return count
     }
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .Delete
+    }
     func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as? OneThirdContentCell
+        if(cell == nil)
+        {
+            return false
+        }
+        if(cell!.isDeletable)
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+        
     }
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        
+        if(editingStyle == .Delete)
+        {
+            let cell = tableView.cellForRowAtIndexPath(indexPath) as? OneThirdContentCell
+            cell?.deleteObservation()
+        }
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(contentCellIdentifier, forIndexPath: indexPath) as! OneThirdContentCell
-        
-        cell.clearCell()
         
         let index = indexPath.row + 1
         let obsType = ObservationTabularViewRow(rawValue: index)
